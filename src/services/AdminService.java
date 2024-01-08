a1eb33bf036d939ce06c4498f54760e0a05166b6
Béla Jash <abelxotessema@gmail.com>
2024-01-08 20:20:47 +0300

    Added admins list and its initializer method in services.AdminService;

--- src/services/AdminService.java
+++ src/services/AdminService.java
@@ -1,4 +1,15 @@
 package services;
 
+import models.Admin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminService {
+    protected List<Admin> admins = new ArrayList<>();
+
+    protected void initializeAdminsList() {
+        // ToDo: Read all admins from file and add them to the admins List
+    }
+
 }
