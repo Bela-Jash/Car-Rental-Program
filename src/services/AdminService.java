44e7e060decfd8d54f497a66c57455d2023780a3
Béla Jash <abelxotessema@gmail.com>
2024-01-12 00:27:00 +0300

    Added a getter for the admins List;

--- src/services/AdminService.java
+++ src/services/AdminService.java
@@ -6,10 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdminService {
+
     protected List<Admin> admins = new ArrayList<>();
 
     protected void initializeAdminsList() {
         // ToDo: Read all admins from file and add them to the admins List
     }
 
+    public List<Admin> getAdmins() {
+        initializeAdminsList();
+        return admins;
+    }
 }
