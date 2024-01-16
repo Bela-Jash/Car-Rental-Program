32ae88b7e46d40cecb4bfce2734b5b1fa79a7845
Béla Jash <abelxotessema@gmail.com>
2024-01-16 11:40:36 +0300

    Made it to function partially

--- src/programs/OwnerProgram.java
+++ src/programs/OwnerProgram.java
@@ -1,7 +1,13 @@
 package programs;
 
+import screen_managers.OwnerScreenManager;
+import screen_managers.OwnerScreenManagerImplementer;
+
 public abstract class OwnerProgram {
     public static void main(String[] args) {
-
+        OwnerScreenManager manager = new OwnerScreenManagerImplementer();
+        manager.switchScreen("WelcomeScreen");
+        System.out.println();
+        System.out.println("Thank you for using this program. See you soon!");
     }
 }
