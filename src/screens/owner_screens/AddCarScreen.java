0b15b78844a3f74d1e6229d00c999e01c20b4664
Béla Jash <abelxotessema@gmail.com>
2024-01-19 18:58:56 +0300

    Improved the correctionText for inputs that don't satisfy the car fields patterns

--- src/screens/owner_screens/AddCarScreen.java
+++ src/screens/owner_screens/AddCarScreen.java
@@ -18,10 +18,10 @@ public class AddCarScreen {
     public void display() {
         System.out.println();
         System.out.println("... > Manage Cars > Add Car");
-        String brand = Console.readText("Enter the brand: ", Patterns.carBrandPattern, "Invalid format or Too few or too many characters. Please try again.");
-        String model = Console.readText("Enter the model: ", Patterns.carModelPattern, "Invalid format or Too few or too many characters. Please try again.");
-        String type = Console.readText("Enter the type: ", Patterns.carTypePattern, "Invalid format or Too few or too many characters. Please try again.");
-        String color = Console.readText("Enter the color: ", Patterns.carColorPattern, "Invalid format or Too few or too many characters. Please try again.");
+        String brand = Console.readText("Enter the brand: ", Patterns.carBrandPattern, "Invalid format or too few or too many characters. Please try again.");
+        String model = Console.readText("Enter the model: ", Patterns.carModelPattern, "Invalid format or too few or too many characters. Please try again.");
+        String type = Console.readText("Enter the type: ", Patterns.carTypePattern, "Invalid format or too few or too many characters. Please try again.");
+        String color = Console.readText("Enter the color: ", Patterns.carColorPattern, "Invalid format or too few or too many characters. Please try again.");
         int year = (int) Console.readNumber("Enter the year: ", 1900, LocalDate.now().getYear());
         int quantityAvailable = (int) Console.readNumber("Enter the quantity available: ", 0, 999);
         double baseRate = Console.readNumber("Enter the base rate: ", 1, 10_000_000);
