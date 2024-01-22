e67e92494103cfd4d3e750ece8f7f8ca7c408dcd
Béla Jash <abelxotessema@gmail.com>
2024-01-22 14:18:26 +0300

    Adding file handling, round 1

--- src/screens/owner_screens/ChangeBaseRateScreen.java
+++ src/screens/owner_screens/ChangeBaseRateScreen.java
@@ -2,12 +2,14 @@ package screens.owner_screens;
 
 import models.Car;
 import screen_managers.OwnerScreenManager;
+import services.CarService;
 import utility.Console;
 
 import java.util.regex.Pattern;
 
 public class ChangeBaseRateScreen {
     private final OwnerScreenManager ownerScreenManager;
+    private final CarService carService = new CarService();
 
     public ChangeBaseRateScreen(OwnerScreenManager ownerScreenManager) {
         this.ownerScreenManager = ownerScreenManager;
@@ -16,21 +18,29 @@ public class ChangeBaseRateScreen {
     public void display() {
         System.out.println();
         System.out.println("... > Modify Car > Base Rate");
-        Car car = new Car();
-        // ToDo: Fetch car id from CarToBeModified.txt or something, and assign it to car
-        System.out.println("The previous base rate is: " + car.getBaseRate() + ".");
-        double newBaseRate = Console.readNumber("Enter the new base rate", 1, 10_000_000);
-        System.out.println("The base rate of this car will henceforth be: Br. " + newBaseRate + ".");
-        System.out.println("Are you sure you want to change it to that?");
-        String choice = Console.readText("Choice (Y/N)", Pattern.compile("[YyNn]"), "Invalid choice. Please try again.");
-        if (choice.equalsIgnoreCase("y")) {
-            car.setBaseRate(newBaseRate);
-            // ToDo: Update the file containing this specific car
-            System.out.println("The base rate has been successfully changed to Br. " + newBaseRate + ".");
-        } else {
-            System.out.println("You have chosen not to change the base rate.");
+
+        // ToDo (Done): Fetch car id from CarToBeModified.txt or something, and assign it to car
+        int toBeModifiedCarId = carService.fetchToBeModifiedCarId();
+        if (toBeModifiedCarId == -1) {
+            System.out.println("Could not fetch the ID number of the car to be modified. Please try again.");
         }
 
+        else {
+            Car car = carService.fetchCarById(toBeModifiedCarId);
+
+            System.out.println("The previous base rate is: " + car.getBaseRate() + ".");
+            double newBaseRate = Console.readNumber("Enter the new base rate", 1, 10_000_000);
+            System.out.println("The base rate of this car will henceforth be: Br. " + newBaseRate + ".");
+            System.out.println("Are you sure you want to change it to that?");
+            String choice = Console.readText("Choice (Y/N)", Pattern.compile("[YyNn]"), "Invalid choice. Please try again.");
+            if (choice.equalsIgnoreCase("y")) {
+                car.setBaseRate(newBaseRate);
+                // ToDo: Update the file containing this specific car
+                System.out.println("The base rate has been successfully changed to Br. " + newBaseRate + ".");
+            } else {
+                System.out.println("You have chosen not to change the base rate.");
+            }
+        }
         Console.continueOnEnter();
         ownerScreenManager.switchScreen("ModifyCarScreen");
     }
--- src/screens/owner_screens/ChangeYearScreen.java
+++ src/screens/owner_screens/ChangeYearScreen.java
@@ -2,6 +2,7 @@ package screens.owner_screens;
 
 import models.Car;
 import screen_managers.OwnerScreenManager;
+import services.CarService;
 import utility.Console;
 import utility.Patterns;
 
@@ -10,6 +11,7 @@ import java.util.regex.Pattern;
 
 public class ChangeYearScreen {
     private final OwnerScreenManager ownerScreenManager;
+    private final CarService carService = new CarService();
 
     public ChangeYearScreen(OwnerScreenManager ownerScreenManager) {
         this.ownerScreenManager = ownerScreenManager;
@@ -18,21 +20,28 @@ public class ChangeYearScreen {
     public void display() {
         System.out.println();
         System.out.println("... > Modify Car > Year");
-        Car car = new Car();
-        // ToDo: Fetch car id from CarToBeModified.txt or something, and assign it to car
-        System.out.println("The previous year is: " + car.getYear() + ".");
-        int newYear = (int) Console.readNumber("Enter the new year", 1900, LocalDate.now().getYear());
-        System.out.println("The year of this car will henceforth be: " + newYear + ".");
-        System.out.println("Are you sure you want to change it to that?");
-        String choice = Console.readText("Choice (Y/N)", Pattern.compile("[YyNn]"), "Invalid choice. Please try again.");
-        if (choice.equalsIgnoreCase("y")) {
-            car.setYear(newYear);
-            // ToDo: Update the file containing this specific car
-            System.out.println("The year has been successfully changed to \"" + newYear + "\".");
-        } else {
-            System.out.println("You have chosen not to change the year.");
-        }
 
+        // ToDo: Fetch car id from CarToBeModified.txt or something, and assign it to car
+        int toBeModifiedCarId = carService.fetchToBeModifiedCarId();
+        if (toBeModifiedCarId == -1) {
+            System.out.println("Could not fetch the ID number of the car to be modified. Please try again.");
+        }
+        else {
+            Car car = carService.fetchCarById(toBeModifiedCarId);
+
+            System.out.println("The previous year is: " + car.getYear() + ".");
+            int newYear = (int) Console.readNumber("Enter the new year", 1900, LocalDate.now().getYear());
+            System.out.println("The year of this car will henceforth be: " + newYear + ".");
+            System.out.println("Are you sure you want to change it to that?");
+            String choice = Console.readText("Choice (Y/N)", Pattern.compile("[YyNn]"), "Invalid choice. Please try again.");
+            if (choice.equalsIgnoreCase("y")) {
+                car.setYear(newYear);
+                // ToDo: Update the file containing this specific car
+                System.out.println("The year has been successfully changed to \"" + newYear + "\".");
+            } else {
+                System.out.println("You have chosen not to change the year.");
+            }
+        }
         Console.continueOnEnter();
         ownerScreenManager.switchScreen("ModifyCarScreen");
     }
